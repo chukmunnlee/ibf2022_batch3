4bf4ac4eee155d8e8abefe593038eb292dbb3973
chukmunnlee <chukmunnlee@gmail.com>
2023-04-24 08:47:46 +0800

    day25 revision

---
+++ day25/src/main/java/ibf2022/batch3/paf/day25/Day25RevisionApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.batch3.paf.day25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day25RevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day25RevisionApplication.class, args);
+	}
+
+}
---
+++ day25/src/test/java/ibf2022/batch3/paf/day25/Day25RevisionApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.batch3.paf.day25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day25RevisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
