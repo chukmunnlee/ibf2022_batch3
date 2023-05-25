8c061d6f4d860cfe67e85a8b3ff7db35b28dc338
chukmunnlee <chukmunnlee@gmail.com>
2023-05-25 08:46:43 +0800

    day39

---
+++ day39/server/src/main/java/ibf2022/batch3/csf/day39/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.batch3.csf.day39.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
